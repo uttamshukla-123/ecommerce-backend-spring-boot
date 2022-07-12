f98ba91d652b810624ed7c7267455248a10153b0
uttamshukla123 <uttamshukla12345@gmail.com>
2022-07-12 14:25:52 +0530

    Added the Backend Support

---
+++ src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.ecommerce.entity.ProductCategory;
+
+@CrossOrigin("http://localhost:5000")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
