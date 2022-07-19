68e5c36b9f3cbddd1fa495f16aa86a398b1fd8e0
uttamshukla123' <uttamshukla12345@gmail.com>
2022-07-19 11:15:41 +0530

    Added the Security for Orders

---
+++ src/main/java/com/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.okta.spring.boot.oauth.Okta;
+
+@Configuration
+public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//protect endpoints /api/orders
+		http.authorizeRequests()
+		.antMatchers("/api/orders/**")
+		.authenticated()
+		.and()
+		.oauth2ResourceServer()
+		.jwt();
+		
+		//add cors filters
+		http.cors();
+		
+		//force a non empty body for 401 to make the response
+		Okta.configureResourceServer401ResponseBody(http);
+	}
+	
+	
+
+}
